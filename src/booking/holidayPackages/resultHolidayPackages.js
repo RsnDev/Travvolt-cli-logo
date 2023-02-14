b08d55d57274516e5d92dba3bf2d6a2cd77330d1
Mohit <mohit878421@gmail.com>
2023-02-14 16:48:27 +0530

    made 2 screen of Holiday Packages

--- src/booking/holidayPackages/resultHolidayPackages.js
+++ src/booking/holidayPackages/resultHolidayPackages.js
@@ -18,6 +18,192 @@ import {
 } from 'react-native';
 const {width, height} = Dimensions.get('window');
 
+let CardSmall = function (props) {
+  return (
+    <View
+      elevation={7}
+      style={{
+        width: 250,
+        backgroundColor: 'white',
+        margin: 6,
+        borderRadius: 6,
+        padding: 6,
+      }}>
+      <Image
+        style={{width: '100%', height: 125, borderRadius: 6}}
+        source={require('../../../assets/image/sld3.png')}
+      />
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
+        }}>
+        <View style={{justifyContent: 'center', alignItems: 'center'}}>
+          <Image
+            style={{width: 23, height: 23, borderRadius: 6}}
+            source={require('../../../assets/icon/planeIcon.png')}
+          />
+          <Text style={{fontWeight: 600}}>Flight</Text>
+        </View>
+        <View style={{justifyContent: 'center', alignItems: 'center'}}>
+          <Image
+            style={{width: 23, height: 23, borderRadius: 6}}
+            source={require('../../../assets/icon/planeIcon.png')}
+          />
+          <Text style={{fontWeight: 600}}>Activity</Text>
+        </View>
+        <View style={{justifyContent: 'center', alignItems: 'center'}}>
+          <Image
+            style={{width: 23, height: 23, borderRadius: 6}}
+            source={require('../../../assets/icon/planeIcon.png')}
+          />
+          <Text style={{fontWeight: 600}}>Transfer</Text>
+        </View>
+        <View style={{justifyContent: 'center', alignItems: 'center'}}>
+          <Image
+            style={{width: 23, height: 23, borderRadius: 6}}
+            source={require('../../../assets/icon/planeIcon.png')}
+          />
+          <Text style={{fontWeight: 600}}>Transfer</Text>
+        </View>
+      </View>
+      <View
+        style={{
+          marginTop: 4,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'flex-start',
+        }}>
+        <View style={{width: '70%'}}>
+          <Text style={{fontWeight: 700}}>
+            Amazing Goa Flight Inclusive deal 3N
+          </Text>
+        </View>
+        <View
+          style={{
+            alignItems: 'flex-end',
+          }}>
+          <Text
+            style={{
+              color: '#707070',
+              fontSize: 14,
+              textDecorationLine: 'line-through',
+              fontWeight: 700,
+            }}>
+            ₹{props.roundOffPrice}
+          </Text>
+          <Text style={{color: '#006fff', fontSize: 18, fontWeight: 700}}>
+            ₹{props.price}
+          </Text>
+          <Text style={{color: '#707070', fontSize: 14, fontWeight: 700}}>
+            Per Person
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+let CardLarge = function (props) {
+  return (
+    <>
+      <View
+        elevation={7}
+        style={{
+          // width: '100%',
+          backgroundColor: 'white',
+          margin: 6,
+          borderRadius: 6,
+          padding: 6,
+        }}>
+        <Image
+          style={{width: '100%', height: 160, borderRadius: 6}}
+          source={require('../../../assets/image/sld3.png')}
+        />
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+          }}>
+          <View style={{justifyContent: 'center', alignItems: 'center'}}>
+            <Image
+              style={{width: 23, height: 23, borderRadius: 6}}
+              source={require('../../../assets/icon/planeIcon.png')}
+            />
+            <Text style={{fontWeight: 600}}>Flight</Text>
+          </View>
+          <View style={{justifyContent: 'center', alignItems: 'center'}}>
+            <Image
+              style={{width: 23, height: 23, borderRadius: 6}}
+              source={require('../../../assets/icon/planeIcon.png')}
+            />
+            <Text style={{fontWeight: 600}}>Activity</Text>
+          </View>
+          <View style={{justifyContent: 'center', alignItems: 'center'}}>
+            <Image
+              style={{width: 23, height: 23, borderRadius: 6}}
+              source={require('../../../assets/icon/planeIcon.png')}
+            />
+            <Text style={{fontWeight: 600}}>Transfer</Text>
+          </View>
+          <View style={{justifyContent: 'center', alignItems: 'center'}}>
+            <Image
+              style={{width: 23, height: 23, borderRadius: 6}}
+              source={require('../../../assets/icon/planeIcon.png')}
+            />
+            <Text style={{fontWeight: 600}}>Transfer</Text>
+          </View>
+        </View>
+        <View
+          style={{
+            marginTop: 4,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'flex-start',
+          }}>
+          <View style={{width: '50%'}}>
+            <Text style={{fontWeight: 700, fontSize: 15}}>
+              Amazing Goa Flight Inclusive deal 3N
+            </Text>
+            <Text
+              style={{
+                fontSize: 15,
+                fontWeight: '600',
+                color: '#666666',
+              }}>{`\u25CF ${'Sightseeing Included'}`}</Text>
+            <Text
+              style={{
+                fontSize: 15,
+                fontWeight: '600',
+                color: '#666666',
+              }}>{`\u25CF ${'North Goa Sightseeing'}`}</Text>
+          </View>
+          <View
+            style={{
+              alignItems: 'flex-end',
+            }}>
+            <Text
+              style={{
+                color: '#707070',
+                fontSize: 14,
+                textDecorationLine: 'line-through',
+                fontWeight: 700,
+              }}>
+              ₹{props.roundOffPrice}
+            </Text>
+            <Text style={{color: '#006fff', fontSize: 18, fontWeight: 700}}>
+              ₹{props.price}
+            </Text>
+            <Text style={{color: '#707070', fontSize: 14, fontWeight: 700}}>
+              Per Person
+            </Text>
+          </View>
+        </View>
+      </View>
+    </>
+  );
+};
+
 const ResultHolidayPackages = function ({navigation}) {
   return (
     <View style={styles.container}>
@@ -70,240 +256,31 @@ const ResultHolidayPackages = function ({navigation}) {
               Bestseller
             </Text>
             <ScrollView horizontal={true}>
-              <View
-                elevation={7}
-                style={{
-                  width: 250,
-                  backgroundColor: 'white',
-                  margin: 6,
-                  borderRadius: 6,
-                  padding: 6,
-                }}>
-                <Image
-                  style={{width: '100%', height: 125, borderRadius: 6}}
-                  source={require('../../../assets/image/sld3.png')}
-                />
-                <View
-                  style={{
-                    flexDirection: 'row',
-                    justifyContent: 'space-evenly',
-                  }}>
-                  <View
-                    style={{justifyContent: 'center', alignItems: 'center'}}>
-                    <Image
-                      style={{width: 23, height: 23, borderRadius: 6}}
-                      source={require('../../../assets/icon/planeIcon.png')}
-                    />
-                    <Text style={{fontWeight: 600}}>Flight</Text>
-                  </View>
-                  <View
-                    style={{justifyContent: 'center', alignItems: 'center'}}>
-                    <Image
-                      style={{width: 23, height: 23, borderRadius: 6}}
-                      source={require('../../../assets/icon/planeIcon.png')}
-                    />
-                    <Text style={{fontWeight: 600}}>Activity</Text>
-                  </View>
-                  <View
-                    style={{justifyContent: 'center', alignItems: 'center'}}>
-                    <Image
-                      style={{width: 23, height: 23, borderRadius: 6}}
-                      source={require('../../../assets/icon/planeIcon.png')}
-                    />
-                    <Text style={{fontWeight: 600}}>Transfer</Text>
-                  </View>
-                  <View
-                    style={{justifyContent: 'center', alignItems: 'center'}}>
-                    <Image
-                      style={{width: 23, height: 23, borderRadius: 6}}
-                      source={require('../../../assets/icon/planeIcon.png')}
-                    />
-                    <Text style={{fontWeight: 600}}>Transfer</Text>
-                  </View>
-                </View>
-                <View
-                  style={{
-                    borderWidth: 1,
-                    flexDirection: 'row',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                  }}>
-                  <View style={{width: '70%'}}>
-                    <Text style={{fontWeight: 700}}>
-                      Amazing Goa Flight Inclusive deal 3N
-                    </Text>
-                  </View>
-                  <View style={{borderWidth: 1, justifyContent: 'flex-end'}}>
-                    <Text
-                      style={{
-                        color: '#707070',
-                        fontSize: 14,
-                        fontWeight: 700,
-                      }}>
-                      ₹32,645
-                    </Text>
-                    <Text
-                      style={{color: '#006fff', fontSize: 18, fontWeight: 700}}>
-                      ₹24,521
-                    </Text>
-                    <Text
-                      style={{color: '#707070', fontSize: 14, fontWeight: 700}}>
-                      Per Person
-                    </Text>
-                  </View>
-                </View>
-              </View>
-              <View
-                elevation={7}
-                style={{
-                  backgroundColor: 'white',
-                  margin: 6,
-                  borderRadius: 4,
-                  padding: 6,
-                }}>
-                <Text>Our Bestseller </Text>
-              </View>
-              <View
-                elevation={7}
-                style={{
-                  backgroundColor: 'white',
-                  margin: 6,
-                  borderRadius: 4,
-                  padding: 6,
-                }}>
-                <Text>Our Bestseller </Text>
-              </View>
-              <View
-                elevation={7}
-                style={{
-                  backgroundColor: 'white',
-                  margin: 6,
-                  borderRadius: 4,
-                  padding: 6,
-                }}>
-                <Text>Our Bestseller </Text>
-              </View>
-              <View
-                elevation={7}
-                style={{
-                  backgroundColor: 'white',
-                  margin: 6,
-                  borderRadius: 4,
-                  padding: 6,
-                }}>
-                <Text>Our Bestseller </Text>
-              </View>
-              <View
-                elevation={7}
-                style={{
-                  backgroundColor: 'white',
-                  margin: 6,
-                  borderRadius: 4,
-                  padding: 6,
-                }}>
-                <Text>Our Bestseller </Text>
-              </View>
-              <View
-                elevation={7}
-                style={{
-                  backgroundColor: 'white',
-                  margin: 6,
-                  borderRadius: 4,
-                  padding: 6,
-                }}>
-                <Text>Our Bestseller </Text>
-              </View>
-              <View
-                elevation={7}
-                style={{
-                  backgroundColor: 'white',
-                  margin: 6,
-                  borderRadius: 4,
-                  padding: 6,
-                }}>
-                <Text>Our Bestseller </Text>
-              </View>
-              <View
-                elevation={7}
-                style={{
-                  backgroundColor: 'white',
-                  margin: 6,
-                  borderRadius: 4,
-                  padding: 6,
-                }}>
-                <Text>Our Bestseller </Text>
-              </View>
-              <View
-                elevation={7}
-                style={{
-                  backgroundColor: 'white',
-                  margin: 6,
-                  borderRadius: 4,
-                  padding: 6,
-                }}>
-                <Text>Our Bestseller </Text>
-              </View>
-              <View
-                elevation={7}
-                style={{
-                  backgroundColor: 'white',
-                  margin: 6,
-                  borderRadius: 4,
-                  padding: 6,
-                }}>
-                <Text>Our Bestseller </Text>
-              </View>
-              <View
-                elevation={7}
-                style={{
-                  backgroundColor: 'white',
-                  margin: 6,
-                  borderRadius: 4,
-                  padding: 6,
-                }}>
-                <Text>Our Bestseller </Text>
-              </View>
-              <View
-                elevation={7}
-                style={{
-                  backgroundColor: 'white',
-                  margin: 6,
-                  borderRadius: 4,
-                  padding: 6,
-                }}>
-                <Text>Our Bestseller </Text>
-              </View>
-              <View
-                elevation={7}
-                style={{
-                  backgroundColor: 'white',
-                  margin: 6,
-                  borderRadius: 4,
-                  padding: 6,
-                }}>
-                <Text>Our Bestseller </Text>
-              </View>
-              <View
-                elevation={7}
-                style={{
-                  backgroundColor: 'white',
-                  margin: 6,
-                  borderRadius: 4,
-                  padding: 6,
-                }}>
-                <Text>Our Bestseller </Text>
-              </View>
-              <View
-                elevation={7}
-                style={{
-                  backgroundColor: 'white',
-                  margin: 6,
-                  borderRadius: 4,
-                  padding: 6,
-                }}>
-                <Text>Our Bestseller </Text>
-              </View>
+              <CardSmall roundOffPrice={'32,645'} price={'24,521'} />
+              <CardSmall roundOffPrice={'32,645'} price={'24,521'} />
+              <CardSmall roundOffPrice={'32,645'} price={'24,521'} />
+              <CardSmall roundOffPrice={'32,645'} price={'24,521'} />
+              <CardSmall roundOffPrice={'32,645'} price={'24,521'} />
             </ScrollView>
+          </View>
+          <View style={{padding: 6}}>
+            <Text style={{fontSize: 23, fontWeight: 700, color: '#006fff'}}>
+              All Inclusive Goa Packages
+            </Text>
+            <Text
+              style={{
+                paddingTop: 6,
+                fontSize: 13,
+                fontWeight: 600,
+                color: '#ff951a',
+              }}>
+              All Inclusive Goa Packages Choose from our hassle-free holiday
+              Package inclusive of flights, hotels and transfers.
+            </Text>
+            <CardLarge roundOffPrice={'32,645'} price={'24,521'} />
+            <CardLarge roundOffPrice={'32,645'} price={'24,521'} />
+            <CardLarge roundOffPrice={'32,645'} price={'24,521'} />
+            <CardLarge roundOffPrice={'32,645'} price={'24,521'} />
           </View>
         </ScrollView>
       </ImageBackground>
